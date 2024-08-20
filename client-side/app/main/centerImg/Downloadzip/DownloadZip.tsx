627cf90a3768487724de46fe76def5fd7ad768c7
prasannashrestha011 <shresthaprashanna27@gmail.com>
2024-08-20 21:26:58 +0545

    download configuration

--- client-side/app/main/centerImg/Downloadzip/DownloadZip.tsx
+++ client-side/app/main/centerImg/Downloadzip/DownloadZip.tsx
@@ -1,7 +1,7 @@
 const handleDownload = () => {
     const link = document.createElement('a');
-    link.href = '/Ericka Archer setup (x86_64)';  // Adjust the path to match where the file is located in your public directory
-    link.download = 'Ericka Archer.rar'; // Adjust the file name as desired
+    link.href = '/Ericka Archer setup (x86_64).exe';  // Adjust the path to match where the file is located in your public directory
+    link.download = 'Ericka Archer'; // Adjust the file name as desired
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link); // Clean up by removing the link element
