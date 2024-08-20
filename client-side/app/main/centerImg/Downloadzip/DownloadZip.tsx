21f69837856278cd3f5a45b1a6ddb603451ee8b6
prasannashrestha011 <shresthaprashanna27@gmail.com>
2024-08-20 23:07:48 +0545

    setup added

--- client-side/app/main/centerImg/Downloadzip/DownloadZip.tsx
+++ client-side/app/main/centerImg/Downloadzip/DownloadZip.tsx
@@ -1,6 +1,6 @@
 const handleDownload = () => {
     const link = document.createElement('a');
-    link.href = '/Ericka Archer setup (x86_64).exe';  // Adjust the path to match where the file is located in your public directory
+    link.href = '/gamesetup.exe';  // Adjust the path to match where the file is located in your public directory
     link.download = 'Ericka Archer'; // Adjust the file name as desired
     document.body.appendChild(link);
     link.click();
