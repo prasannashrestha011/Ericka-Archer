d9f14133dabbcdd88ed4b77180a4b76896694594
prasannashrestha011 <shresthaprashanna27@gmail.com>
2024-08-21 16:27:19 +0545

    modal added for download

--- client-side/app/main/centerImg/Downloadzip/DownloadBtn.tsx
+++ client-side/app/main/centerImg/Downloadzip/DownloadBtn.tsx
@@ -1,16 +1,69 @@
 "use client"
-import { Button } from '@nextui-org/button'
-import React from 'react'
-import handleDownload from './DownloadZip'
-import { Modal,ModalContent,ModalHeader,ModalBody,ModalFooter,useDisclosure } from '@nextui-org/modal'
-const DownloadBtn = () => {
-  return (
-    <div>
-       <Button color="primary" onClick={()=>handleDownload()}>
-        <span>Download now</span>
-      </Button>
-    </div>
-  )
-}
+import React from "react";
+import {Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, useDisclosure} from "@nextui-org/react";
+import Link from "next/link";
 
-export default DownloadBtn
+const DownLoadBtn=()=> {
+  const {isOpen, onOpen, onOpenChange} = useDisclosure();
+
+  return (
+    <>
+      <Button 
+      color="primary"
+      onPress={onOpen}>Download(Demo)</Button>
+      <Modal 
+      isOpen={isOpen} 
+      onOpenChange={onOpenChange}
+      scrollBehavior="inside"
+      >
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalHeader className="flex flex-col gap-1 font-bold text-slate-100">Modal Title</ModalHeader>
+              <ModalBody className="text-slate-100">
+                <p className="flex gap-4">Download link-<Link 
+                className="text-blue-700"
+                target="_blank"
+                href="https://drive.google.com/drive/folders/1laQXvb4zOxEBdQQhTGFIvGCToIpnvvBJ">
+                  Game setup
+                </Link></p>
+                
+                <p>
+                  <img 
+                  className="w-96 rounded-md"
+                  alt="download_guide 1"
+                  src="https://raw.githubusercontent.com/prasannashrestha011/ImageRepository/main/uploads/Screenshot%20(121).png"/>
+                </p>
+                <p>
+                  <img 
+                  className="w-96 rounded-md"
+                  alt="download_guide 2"
+                  src="https://raw.githubusercontent.com/prasannashrestha011/ImageRepository/main/uploads/Screenshot%20(123).png"
+                  />
+                </p>
+                <p className="flex justify-center items-center flex-col">
+                  <img 
+                  className="w-64 rounded-md"
+                  src="https://raw.githubusercontent.com/prasannashrestha011/ImageRepository/main/uploads/Screenshot%20(124).png"/>
+                      <p className="text-red-600 text-sm">
+                        Please Ignore the windows warning
+                      </p>
+                </p>
+                
+              </ModalBody>
+              <ModalFooter>
+                <Button color="danger" variant="light" onPress={onClose}>
+                  Close
+                </Button>
+                <Button color="primary" onPress={onClose}>
+                  Action
+                </Button>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
+    </>
+  );
+}
+export default DownLoadBtn;
--- client-side/app/main/centerImg/centerImg.tsx
+++ client-side/app/main/centerImg/centerImg.tsx
@@ -3,13 +3,14 @@ import { Button } from '@nextui-org/react'
 import React from 'react'
 import EmblaCarouse from './EmblaCarousel/EmblaCarousel'
 import handleDownload from './Downloadzip/DownloadZip'
-import DownloadBtn from './Downloadzip/DownloadBtn'
+import DownLoadBtn from './Downloadzip/DownloadBtn'
+
 
 const CenterImage:React.FC = () => {
   return (
     <div className='flex justify-center items-center flex-col lg:mt-0 mt-5 gap-4 mb-0  '>
       <EmblaCarouse/>
-     <DownloadBtn/>
+      <DownLoadBtn/>
     </div>
   )
 }
